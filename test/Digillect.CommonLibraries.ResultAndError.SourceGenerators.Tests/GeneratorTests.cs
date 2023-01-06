56e452e3a87fc5d155c8a55973cc5fb41e79e081
Gregory Nickonov <gregory@up-up.agency>
2023-01-07 00:03:51 +0300

    down

--- test/Digillect.CommonLibraries.ResultAndError.SourceGenerators.Tests/GeneratorTests.cs
+++ test/Digillect.CommonLibraries.ResultAndError.SourceGenerators.Tests/GeneratorTests.cs
@@ -1,7 +1,6 @@
 using System.Reflection;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-using Xunit;
 
 namespace Digillect.CommonLibraries.ResultAndError.SourceGenerators.Tests;
 
